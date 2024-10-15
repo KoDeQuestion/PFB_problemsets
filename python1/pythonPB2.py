26c75b021f9b8efea6c115e1159cc18569b1e2a0
pfb2024 <pfb2024@info08.cshl.edu>
2024-10-15 15:41:10 -0400

    PythonPB2 PartII

---
+++ python1/pythonPB2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+var = 20
+if var < 100 :
+	message = 'is less than 100. It's basically free.'
+	print(var, message)
+elif var > 100 :
+	message = 'is greater than 100. You're too poor.'
+	print(var, message)
+else:
+	message = 'is equal to 100. Better think about it some more.'
+	print(var, message)
+
+
+
