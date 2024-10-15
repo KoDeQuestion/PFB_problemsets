b3cac079c04d1054df563bec535f8531becbf69a
pfb2024 <pfb2024@info08.cshl.edu>
2024-10-15 12:01:09 -0400

    Adding git-ignore to stop it from freaking out about swap files as well as the Plus Separated vs Comma Separated Python1 file

--- python1/about_me.py
+++ python1/about_me.py
@@ -4,4 +4,4 @@ name = sys.argv[1]
 color = sys.argv[2]
 activity = sys.argv[3]
 animal = sys.argv[4]
-print('My name:',name,'\nMy favorite color:',color,'\nMy favorite activity',activity,'\nMy favorite animal:',animal)
+print('My name:',name,'\nMy favorite color:',color,'\nMy favorite activity:',activity,'\nMy favorite animal:',animal)
