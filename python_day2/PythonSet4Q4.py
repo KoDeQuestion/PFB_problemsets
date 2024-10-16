2fce6806de7b197a605cff9d663177afa3690e0b
pfb2024 <pfb2024@info08.cshl.edu>
2024-10-16 16:45:12 -0400

    Added PySet4Q4

---
+++ python_day2/PythonSet4Q4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#Python Problem Set 4 - Question 4:
+
+import sys
+
+count = 0
+
+while count < 100:
+	count+=1
+	print(count)
+
+#print("Done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
