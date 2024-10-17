f18dfd7e080e27a64c9d35ae8dd6cff50fca3f9f
pfb2024 <pfb2024@info08.cshl.edu>
2024-10-16 21:27:36 -0400

    Challenge Problem 1 Added

--- python_day2/PythonSet4Q17.py
+++ python_day2/PythonSet4Q17.py
@@ -13,3 +13,41 @@
 #    Print the final shuffled sequence
 #    Remember to test your code with test data.
 
+import sys
+from random import *
+
+#x = sys.argv[1]
+
+seq = '123456'
+n = len(seq)
+
+#convert seq to list
+
+#Generate the random index
+#random_index = randrange(len(seq))
+#print(random_index)
+
+seq = '67898'
+n = len(seq)
+newlist = []
+for _ in seq: #I'm not using the variable so _ is a stand in to still use the loop
+    #Generate the random index
+    random_indexA = randrange(1, n)
+    print(f'random_indexA:{random_indexA}')
+    random_indexB = randrange(1, n)
+    print(f'random_indexB:{random_indexB}') #These are already defined positions
+    #Find positions
+    #pos_indexA = seq.find(str(random_indexA))
+    #print(f'pos_indexA:{pos_indexA}')
+    #pos_indexB = seq.find(str(random_indexB))
+    #print(f'pos_indexB:{pos_indexB}')
+    #print(f'Confirm Index Pos A is what we think it is:{seq[pos_indexA]}') #This confirmed that the pos_index actually gives the character at that position (i.e. random_index)
+    #Replace A with B THIS DOES WORK
+    newlist = list(seq)
+    newlist[random_indexA] = seq[random_indexB]
+    newlist[random_indexB] = seq[random_indexA]
+    newseq = ''.join(newlist) #convert list to string
+    print(f'This is the OG {seq}')
+    print(f'This is the new seq {newseq}')
+    #Swap A with B [IN ONE MOVE?]
+    #seq[random_indexA],seq[random_indexB] = seq[random_indexB],seq[random_indexA]
